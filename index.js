ba538591696602ee2fc7bd9718d83c3bee9ddfc3
questcoderull <questcoderull@gmail.com>
2025-08-22 12:58:32 +0600

    solve problem one

--- index.js
+++ index.js
@@ -13,3 +13,17 @@
 
 // Input: "hello"
 // Output: "olleh"
+
+const handleRerseWord = (word) => {
+  const givenWord = word;
+  console.log("given word", givenWord);
+  const makeArry = givenWord.split("");
+  console.log("make array", makeArry);
+  const reverseTheWord = makeArry.reverse();
+  console.log("reversed word", reverseTheWord);
+  const makeString = reverseTheWord.join("");
+  console.log(makeString);
+  return makeString;
+};
+
+handleRerseWord("Hello");
