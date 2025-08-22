84b8a5fac829f67e4685be1feaed39da7436b0dc
questcoderull <questcoderull@gmail.com>
2025-08-22 12:46:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log("hello bhai i am working_ outside");
+// const Check = () => {
+//   console.log("hello bhai i am working_");
+// };
+
+// Check();
+
+//Proble 1.
+// Problem 1: Reverse a String
+// Write a function that takes a string and returns it reversed.
+
+// Example:
+
+// Input: "hello"
+// Output: "olleh"
