6549f3b5c0c726663aea904b5186d921b42d93ee
questcoderull <questcoderull@gmail.com>
2025-08-23 00:30:50 +0600

    problem ten solved

--- index.js
+++ index.js
@@ -225,3 +225,34 @@ const findFactorial = (num) => {
 };
 
 // findFactorial(5);
+
+// Problem 10: PingPong Challenge
+// Write a function that prints numbers from 1 to 20.
+
+// Example:
+
+// For multiples of 3, print "Ping"
+// For multiples of 5, print "Pong"
+// For multiples of both 3 and 5, print "PingPong"
+// If the number is not a multiple of 3 or 5, print the number itself
+// Example Output:
+
+// 1, 2, Ping, 4, Pong, Ping, 7, 8, Ping, Pong, 11, Ping, 13, 14, PingPong, 16 …..
+
+const handlePingPong = (num1, num2) => {
+  for (let i = 1; i <= 20; i++) {
+    // console.log(i);
+
+    if (i % num1 === 0 && i % num2 === 0) {
+      console.log("pingPong");
+    } else if (i % num1 === 0) {
+      console.log("ping");
+    } else if (i % num2 === 0) {
+      console.log("pong");
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+// handlePingPong(3, 5);
